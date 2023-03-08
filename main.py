5fc605bfd1534618c2b1cbe37b66f58bf9f64e37
Clemens Wältken <clemens.waltken@microsoft.com>
2023-03-08 11:23:46 +0100

    Add exception handling

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import sys, signal, time
 
 from azure.identity import DefaultAzureCredential
 from azure.mgmt.subscription import SubscriptionClient
+from azure.core.exceptions import ClientAuthenticationError
 
 
 def main():
@@ -11,11 +12,14 @@ def main():
 
 
 def show_subscription():
-    credential = DefaultAzureCredential()
-    subscription_client = SubscriptionClient(credential)
-    sub_list = subscription_client.subscriptions.list()
-    first_sub = next(sub_list)
-    print(first_sub.subscription_id)
+    try:
+        credential = DefaultAzureCredential()
+        subscription_client = SubscriptionClient(credential)
+        sub_list = subscription_client.subscriptions.list()
+        first_sub = next(sub_list)
+        print(first_sub.subscription_id)
+    except ClientAuthenticationError as e:
+        print(e.exc_msg)
 
 
 def signal_handler(signal, frame):
