eed63b3e657c317b90bbd4f84ae029e7ddb55dd2
Clemens Wältken <clemens.waltken@microsoft.com>
2023-03-08 10:40:37 +0100

    Improve sample program

--- main.py
+++ main.py
@@ -1,14 +1,29 @@
+import sys, signal, time
+
 from azure.identity import DefaultAzureCredential
 from azure.mgmt.subscription import SubscriptionClient
 
 
 def main():
+    while True:
+        show_subscription()
+        time.sleep(10)
+
+
+def show_subscription():
     credential = DefaultAzureCredential()
     subscription_client = SubscriptionClient(credential)
     sub_list = subscription_client.subscriptions.list()
     first_sub = next(sub_list)
     print(first_sub.subscription_id)
 
+
+def signal_handler(signal, frame):
+    print("\nprogram exiting gracefully")
+    sys.exit(0)
+
+
+signal.signal(signal.SIGINT, signal_handler)
 
 if __name__ == "__main__":
     main()
